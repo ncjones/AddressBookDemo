c26ce1ed1d9e19090537d819b92dc33605fd1bdd
Nathan Jones <nathan@ncjones.com>
2012-04-10 21:02:36 +1200

    Add basic Monodevelop GTK# project.

---
+++ AddressBookDemoCSharp/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("AddressBookDemoCSharp")]
+[assembly: AssemblyCopyright("Nathan Jones")]
+[assembly: AssemblyVersion("1.0.*")]
