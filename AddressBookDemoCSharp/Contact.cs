6de3737ecb8b8446f3215121ad34bffa51f106b1
Nathan Jones <nathan@ncjones.com>
2012-04-10 22:50:52 +1200

    Add service interface.

---
+++ AddressBookDemoCSharp/Contact.cs
@@ -0,0 +1,39 @@
+// 
+//  Copyright Nathan Jones 2012
+// 
+//  This file is part of AddressBookDemo.
+// 
+//  AddressBookDemo is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  AddressBookDemo is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with AddressBookDemo.  If not, see <http://www.gnu.org/licenses/>.
+// 
+using System;
+
+namespace AddressBookDemoCSharp
+{
+	public class Contact
+	{
+		public Contact ()
+		{
+		}
+		
+		public int id { get; set; }
+		
+		public string name { get; set; }
+		
+		public string email { get; set; }
+		
+		public string phone { get; set; }
+		
+	}
+}
+
