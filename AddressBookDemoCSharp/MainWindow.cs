06a546d3e614e59a880dd1c725851115dd0a57a0
Nathan Jones <nathan@ncjones.com>
2012-04-10 21:24:33 +1200

    Remove references to gui builder generated artifacts.

--- AddressBookDemoCSharp/MainWindow.cs
+++ AddressBookDemoCSharp/MainWindow.cs
@@ -5,7 +5,16 @@ public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
-		Build ();
+		this.Name = "MainWindow";
+		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
+		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		if ((this.Child != null)) {
+			this.Child.ShowAll ();
+		}
+		this.DefaultWidth = 400;
+		this.DefaultHeight = 300;
+		this.Show ();
+		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
